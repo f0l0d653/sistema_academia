c0a7baebe730251616ebc3dc07b6916a5e3db8bc
Luís Henrique <luishenrique.lhans@gmail.com>
2026-05-23 00:49:26 -0300

    Add Rotas do react

--- Frontand_academia/src/main.tsx
+++ Frontand_academia/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import './index.css'
+import Router from './route'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
 )
